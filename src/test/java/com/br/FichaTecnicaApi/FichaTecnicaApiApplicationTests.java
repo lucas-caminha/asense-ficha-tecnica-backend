8725219adfc3ee2e62d7436c7d04b831a18298db
Danilo Caramelo <danilo.caramelogit@gmail.com>
2021-07-18 08:52:57 -0300

    fix: fixed gitignore

---
+++ src/test/java/com/br/FichaTecnicaApi/FichaTecnicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.FichaTecnicaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaTecnicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
