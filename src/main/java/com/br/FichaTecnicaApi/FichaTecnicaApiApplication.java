d101913c46746281847baed6b086d0572200c44b
lucas-caminha <lucas_santoscaminha@hotmail.com>
2022-06-16 20:00:29 -0300

    adicionado swagger para documentação da api

--- src/main/java/com/br/FichaTecnicaApi/FichaTecnicaApiApplication.java
+++ src/main/java/com/br/FichaTecnicaApi/FichaTecnicaApiApplication.java
@@ -2,14 +2,13 @@ package com.br.FichaTecnicaApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class FichaTecnicaApiApplication {
 
 	public static void main(String[] args) {
